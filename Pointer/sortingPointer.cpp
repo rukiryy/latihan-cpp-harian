c721f3f65db8e837b0233a838e59e98624ff458e
rukiryy <athare.fas@gmail.com>
2025-11-14 15:11:26 +0700

    baru

---
+++ Pointer/sortingPointer.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+void balikArray(int *ptr, int n){
+    int *awal = ptr;
+    int *akhir = ptr;
+    
+}
+
+int main(){
+    int n;
+    cin >> n;
+    int arr[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> *(arr + i);
+    }
+
+    
+}
